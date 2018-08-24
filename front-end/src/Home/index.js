1dd20f458063610d0d9d372fab8b51708f3dd07c
a289900566 <289900566@qq.com>
2018-08-24 10:18:41 +0800

    用图片的方式引入了logo

--- front-end/src/Home/index.js
+++ front-end/src/Home/index.js
@@ -5,8 +5,8 @@ export default class Home extends React.Component {
   render () {
     return (
       <Row style={ style.header }>
-        <Col span={4} style={ style.titleContainer }>
-          <span style={ style.title }>A Blog</span>
+        <Col span={4} style={style.logoContainer}>
+          <img style={ style.logo } src={ require("../assets/blog-logo.png") }></img>
         </Col>
         <Col span={12}></Col>
       </Row>
@@ -19,13 +19,12 @@ const style = {
     height: '70px', 
     borderBottom: '1px solid grey' 
   },
-  titleContainer: { 
-    fontSize: '20px', 
-    color: 'black', 
+  logoContainer: { 
     textAlign: 'center'
   },
-  title: {
-    display: 'inline-block',
+  logo: {
+    width: "50%",
+    height: "50%",
     marginTop: '20px'
   }
 }
